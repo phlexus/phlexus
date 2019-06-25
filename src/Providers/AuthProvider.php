532c26ec9e286b2d9beba213969373de72e55b28
Anton Vasiliev <aionbase@gmail.com>
2019-06-25 18:29:35 +0100

    Implemented custom providers

---
+++ src/Providers/AuthProvider.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+namespace Phlexus\Providers;
+
+use Phlexus\Libraries\Auth\Manager as AuthManager;
+
+class AuthProvider extends AbstractProvider
+{
+    /**
+     * Provider name
+     *
+     * @var string
+     */
+    protected $providerName = 'auth';
+
+    /**
+     * Register application service.
+     *
+     * @param array $parameters Custom parameters for Service Provider
+     * @return void
+     */
+    public function register(array $parameters = [])
+    {
+        $configs = $this->getDI()->getShared('config')->get('auth');
+        $this->di->setShared($this->providerName, function () use ($configs) {
+            return new AuthManager($configs['adapter'], $configs['configurations']);
+        });
+    }
+}
