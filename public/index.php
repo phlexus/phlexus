fcb544849f8babdeec902a8e528ba035a6bcad55
Anton Vasiliev <aionbase@gmail.com>
2019-06-26 22:39:58 +0100

    Added loading of dotenv

--- public/index.php
+++ public/index.php
@@ -1,6 +1,22 @@
 <?php declare(strict_types=1);
 
+/**
+ * Load composer dependencies
+ */
 $loader = require_once __DIR__ . '/../vendor/autoload.php';
+
+/**
+ * Instantiate and load environment variables
+ */
+$dotenv = Dotenv\Dotenv::create(__DIR__ . '/../');
+$dotenv->load();
+
+/**
+ * Load Configurations
+ */
 $configs = require_once __DIR__ . '/../config/config.php';
 
+/**
+ * Instantiate Phlexus Application and run it!
+ */
 echo (new \Phlexus\Application('default', $configs, $loader->getPrefixesPsr4()))->run();
