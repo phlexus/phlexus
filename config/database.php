1f5e46b7f99da3a2985625b2b8f2259e820270c4
Anton Vasiliev <aionbase@gmail.com>
2019-06-26 22:40:08 +0100

    Fixed DB config

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ return [
     'host' => getenv('DB_HOST'),
     'dbname' => getenv('DB_NAME'),
     'port' => getenv('DB_PORT'),
-    'username' => getenv('DB_NAME'),
+    'username' => getenv('DB_USER'),
     'password' => getenv('DB_PASS'),
     'adapter' => getenv('DB_ADAPTER'),
 ];
