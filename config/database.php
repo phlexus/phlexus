9380f10531b4f670a0affaba01c51ee253781be8
Anton Vasiliev <aionbase@gmail.com>
2019-08-13 15:49:29 +0100

    Updated database config for latest phalcon 4

--- config/database.php
+++ config/database.php
@@ -1,10 +1,12 @@
 <?php declare(strict_types=1);
 
 return [
-    'host' => getenv('DB_HOST'),
-    'dbname' => getenv('DB_NAME'),
-    'port' => getenv('DB_PORT'),
-    'username' => getenv('DB_USER'),
-    'password' => getenv('DB_PASS'),
     'adapter' => getenv('DB_ADAPTER'),
+    'options' => [
+        'host' => getenv('DB_HOST'),
+        'dbname' => getenv('DB_NAME'),
+        'port' => getenv('DB_PORT'),
+        'username' => getenv('DB_USER'),
+        'password' => getenv('DB_PASS'),
+    ],
 ];
