4e75304c2fac6a44b9af50a7ee074218d2bedfd0
Anton Vasiliev <aionbase@gmail.com>
2019-08-19 00:50:55 +0100

    Cleanup in routes

--- src/Modules/Admin/Config/routes.php
+++ src/Modules/Admin/Config/routes.php
@@ -4,39 +4,12 @@ use Phalcon\Mvc\Router\Group as RouterGroup;
 
 $routes = new RouterGroup([
     'module' => 'Admin',
-    'controller' => 'index',
-    'action' => 'index',
     'namespace' => 'Phlexus\Modules\Admin\Controllers',
-]);
-
-$routes->addGet('/admin', [
-    'controller' => 1,
-    'action' => 2,
 ]);
 
 $routes->addGet('/admin/users', [
     'controller' => 'users',
     'action' => 'index',
-]);
-
-$routes->addGet('/admin/auth', [
-    'controller' => 'auth',
-    'action' => 'login',
-]);
-
-$routes->addPost('/admin/auth/doLogin', [
-    'controller' => 'auth',
-    'action' => 'doLogin',
-]);
-
-$routes->addGet('/admin/auth/remind', [
-    'controller' => 'auth',
-    'action' => 'remind',
-]);
-
-$routes->addGet('/admin/auth/logout', [
-    'controller' => 'auth',
-    'action' => 'logout',
 ]);
 
 return $routes;
