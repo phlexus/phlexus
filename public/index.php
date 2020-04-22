e76f7c4eb5a1232ec983c91e2d30bb23d6887e71
Anton Vasiliev <aionbase@gmail.com>
2020-04-22 21:02:45 +0100

    Refactor index.php

--- public/index.php
+++ public/index.php
@@ -2,21 +2,37 @@
 declare(strict_types=1);
 
 /**
+ * This file is part of the Phlexus CMS.
+ *
+ * (c) Phlexus CMS <team@phalcon.io>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+use Phlexus\Application;
+
+/**
+ * Define root absolute path
+ */
+$rootPath = dirname(__DIR__);
+
+/**
  * Load composer dependencies
  */
-$loader = require_once __DIR__ . '/../vendor/autoload.php';
+$loader = require_once $rootPath . '/vendor/autoload.php';
 
 /**
  * Instantiate and load environment variables
  */
-Dotenv\Dotenv::create(__DIR__ . '/../')->load();
+Dotenv\Dotenv::create($rootPath)->load();
 
 /**
  * Load Configurations
  */
-$configs = require_once __DIR__ . '/../config/config.php';
+$configs = require_once $rootPath . '/config/config.php';
 
 /**
  * Instantiate Phlexus Application and run it!
  */
-echo (new \Phlexus\Application(\Phlexus\Application::MODE_DEFAULT, $configs, $loader->getPrefixesPsr4()))->run();
+echo (new Application(Application::MODE_DEFAULT, $configs, $loader->getPrefixesPsr4()))->run();
