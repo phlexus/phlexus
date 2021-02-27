c758bf1fafc049dcad253e25f0e783b06a9dea77
Luis Campos <falcon758@users.noreply.github.com>
2021-02-27 00:43:18 +0000

    Code cleaning

--- src/Modules/User/Controllers/UsersController.php
+++ src/Modules/User/Controllers/UsersController.php
@@ -5,7 +5,11 @@ namespace Phlexus\Modules\User\Controllers;
 
 use Phlexus\Modules\BaseUser\Models\Users;
 use Phlexus\Modules\BaseUser\Controllers\AbstractController;
-use \Phlexus\Modules\Generic\Forms\BaseForm;
+use Phlexus\Modules\Generic\Forms\BaseForm;
+use Phalcon\Forms\Element\Email;
+use Phalcon\Forms\Element\Password;
+use Phalcon\Forms\Element\Select;
+use \Phlexus\Modules\BaseUser\Models\Profiles;
 
 final class UsersController extends AbstractController
 {
@@ -26,19 +30,19 @@ final class UsersController extends AbstractController
         $this->setFields([
             [
                 'name' => 'email',
-                'type' => \Phalcon\Forms\Element\Email::class,
+                'type' => Email::class,
                 'required' => true
             ],
             [
                 'name' => 'password',
-                'type' => \Phalcon\Forms\Element\Password::class,
+                'type' => Password::class,
                 'required' => true
             ],
             [
                 'name' => 'profileId',
-                'type' => \Phalcon\Forms\Element\Select::class,
+                'type' => Select::class,
                 'required' => true,
-                'related' => \Phlexus\Modules\BaseUser\Models\Profiles::class,
+                'related' => Profiles::class,
                 'using' => ['id', 'name']
             ]
         ]);
