7f459a97501f86b0e6146bcf0b28fad677f04332
Luis Campos <falcon758@users.noreply.github.com>
2021-03-09 18:47:56 +0000

    Changes to generic structure

---
+++ src/Modules/Generic/Form.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace Phlexus\Modules\Generic;
+
+use Phlexus\Modules\Generic\Forms\BaseForm;
+
+trait Form {
+
+    private BaseForm $form;
+
+    private array $formFields = [];
+
+    private function getForm(): BaseForm {
+        return $this->form;
+    }
+
+    private function setForm(BaseForm $form) {
+        $this->form = $form;
+    }
+
+    private function getFormFields(): array {
+        return $this->formFields;
+    }
+
+    private function setFormFields(array $fields) {
+        $this->formFields = $fields;
+    }
+}
