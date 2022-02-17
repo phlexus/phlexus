a3e221cd12b53b2fa60cd3bf745afc7ef4850d18
Luis Campos <falcon758@users.noreply.github.com>
2022-02-17 12:37:14 +0000

    Fix migration

--- db/migrations/1.0.0/address_type.php
+++ db/migrations/1.0.0/address_type.php
@@ -56,7 +56,7 @@ class AddressTypeMigration_100 extends Migration
             ],
             'options' => [
                 'TABLE_TYPE' => 'BASE TABLE',
-                'AUTO_INCREMENT' => '2',
+                'AUTO_INCREMENT' => '3',
                 'ENGINE' => 'InnoDB',
                 'TABLE_COLLATION' => 'utf8_unicode_ci',
             ],
