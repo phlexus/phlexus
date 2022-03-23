c148cf0e70ce63b924640b6463c3248013a2bcbe
Luis Campos <falcon758@users.noreply.github.com>
2022-03-23 00:02:11 +0000

    New migrations

--- db/migrations/1.0.0/translations.php
+++ db/migrations/1.0.0/translations.php
@@ -99,7 +99,7 @@ class TranslationsMigration_100 extends Migration
             ],
             'options' => [
                 'TABLE_TYPE' => 'BASE TABLE',
-                'AUTO_INCREMENT' => '6',
+                'AUTO_INCREMENT' => '84',
                 'ENGINE' => 'InnoDB',
                 'TABLE_COLLATION' => 'utf8mb4_0900_ai_ci',
             ],
