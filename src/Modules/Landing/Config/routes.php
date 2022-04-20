20d96dcf080dbd303f5934f3e1d9a39afd801c29
Luis Campos <falcon758@users.noreply.github.com>
2022-04-20 18:07:50 +0100

    Code format

--- src/Modules/Landing/Config/routes.php
+++ src/Modules/Landing/Config/routes.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 use Phalcon\Mvc\Router\Group as RouterGroup;
 
 $routes = new RouterGroup([
-    'module' => 'Landing',
+    'module'     => 'Landing',
     'controller' => 'HomeController',
-    'action' => 'index',
-    'namespace' => 'Phlexus\Modules\Landing\Controllers',
+    'action'     => 'index',
+    'namespace'  => 'Phlexus\Modules\Landing\Controllers',
 ]);
 
 $routes->addGet('/', [
     'controller' => 'home',
-    'action' => 'index',
+    'action'     => 'index',
 ]);
 
 return $routes;
