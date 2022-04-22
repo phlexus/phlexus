9c65b67c294ea38963351dc63599a3d491bed44b
Luis Campos <falcon758@users.noreply.github.com>
2022-04-22 18:05:28 +0100

    Add tasks support

---
+++ src/Tasks/SubscriptionTask.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Phlexus\Tasks;
+
+use Phalcon\Cli\Task;
+
+class SubscriptionTask extends Task
+{
+    public function createPaymentsAction()
+    {
+    }
+
+    public function verifySubscriptionAction()
+    {
+    }
+}
